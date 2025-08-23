9c2cfc24badd14e4667f7b9d93141b5b52bfc7d4
abnjv <abwbkralsrajy53@gmail.com>
2025-08-23 17:26:03 +0300

    Revert "Fix deployment, build errors, and test environment"

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/Ha/',
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  css: {
+    postcss: {},
+  },
   server: {
     port: 3000,
   },
